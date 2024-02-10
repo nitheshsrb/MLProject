9d8ebf370d0c706134081627a188d8d7e469a1ac
Nithesh Bharadwaj Sivakumar <55906003+nitheshsrb@users.noreply.github.com>
2024-02-10 23:49:43 +0000

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -3,8 +3,7 @@ import logging
 def error_message_detail(error,error_detail:sys):
         _,_,exc_tb = error_detail.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
-        error_message = "Error occured in python file [{0}] at line [{1}] due to [{2}]".format()
-        file_name,exc_tb.tb_lineno,str(error)
+        error_message = "Error occured in python file [{0}] at line [{1}] due to [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
 
         return error_message
 
