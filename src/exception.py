a8225170f29b864382d03b64a342b0b15b55d36f
nitheshsrb <nitheshbharadwaj45@gmail.com>
2024-02-11 22:33:24 +0000

    Notebook

--- src/exception.py
+++ src/exception.py
@@ -13,12 +13,4 @@ class CustomException(Exception):
                 self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
         def __str__(self):
-                return self.error_message 
-
-if __name__ == "__main__":    
-        try:
-              a=1/0
-
-        except Exception as e: 
-            logging.info("Divide by zero")
-            raise CustomException(e,sys)
+                return self.error_message
